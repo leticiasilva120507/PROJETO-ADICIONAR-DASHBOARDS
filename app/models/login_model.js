cc89ec5765b7c9ceebdc97d56a7b4884a964bf2e
Letícia Silva <rm90116@estudante.fieb.edu.br>
2025-04-30 14:59:20 -0300

    alteração no model

--- app/models/login_model.js
+++ app/models/login_model.js
@@ -1,8 +1,66 @@
 const pool = require("../../config/pool_conexoes");
 const login_model ={
-    create: async (dadosForm) =>{
+    findAll: async() =>{
         try{
-            const[linhas, campos] = await pool.query('INSERT INTO')
+            const [resultados] = await pool.query(
+
+            )
+            return resultados;
+        }catch(error){
+            console.log(error);
+            return error;
         }
-    }
-};
+    },
+
+    findEmail: async (camposForm) =>{
+        try{
+            const [resultados] = await pool.query("SELECT * FROM usuario WHERE email =? ",
+                [camposForm.email, camposForm.email]
+            )
+            return resultados;
+        }catch(error){
+            console.log(error);
+            return error;
+        }
+    },
+
+    findId: async(id) =>{
+        try{
+            const[resultados] = await pool.query("")
+            return resultados;
+        }catch(error){
+            console.log(error);
+            return error;
+        }
+
+    },
+
+    create:async(camposForm) =>{
+        try{
+            const[resultados] = await pool.query(
+                "insert into usuario set?", [camposForm]
+            )
+            return resultados;
+        }catch(error){
+            console.log(error);
+            return null;
+        }
+    },
+
+    update: async (camposForm) =>{
+        try{
+            const[resultados] = await pool.query("UPDATE usuario SET email =?, senha =?"+
+                "WHERE id =?",
+                [camposForm.email, camposForm.camposForm.senha]
+            )
+            return resultados;
+        } catch(error){
+            console.log(error);
+            return error;
+        }
+    },
+
+}
+
+
+module.exports = login_model
