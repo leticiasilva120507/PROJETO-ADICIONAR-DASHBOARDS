22050a1919d78359ef4d439691ae065f206d0d84
Your Name <leticia_silva2007@hotmail.com>
2025-04-30 11:46:08 -0300

    alterei o model, router

--- app/models/login_model.js
+++ app/models/login_model.js
@@ -1,1 +1,8 @@
 const pool = require("../../config/pool_conexoes");
+const login_model ={
+    create: async (dadosForm)=>{
+        try{
+            const[linhas, campos] = await pool.query('INSERT INTO ')
+        }
+    }
+}
--- app/routes/router.js
+++ app/routes/router.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const usuario_Controller = require("../controllers/usuario_controller");
+
+const {body, validationResult} = require("express-validator");
+
+
 
 router.get('/', function(req, res){
     res.render('pages/index', {"erros": null, "valores":{"email":"", "senha":""}});
@@ -9,21 +14,11 @@ router.get('/relatorios', function(req, res){
     res.render('pages/relatorios');
 });
 
-const { body, validationResult} = require("express-validator")
- router.post("/index",
-    body("email").isEmail({allow_display_name: false, require_display_name: false, allow_utf8_local_part: true, require_tld: true, allow_ip_domain:false, allow_underscores: false, domain_specific_validation:false, blacklisted_chars:"", host_blacklist:[]}).withMessage('Digite um email válido!'),
-    body("senha").isStrongPassword({minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1, returnScore: false, pointsPerUnique: 1, pointsPerRepeat: 0.5, pointsForContainingLower: 10, pointsForContainingUpper:10, pointsForContainingNumber:10, pointsForContainingSymbol: 10}).withMessage("A senha está incorreta!"),
+router.post("/", usuario_Controller.regrasValidacao, function(req,res){
+    usuario_Controller.login(req, res);
+});
 
-    function(req, res){
-        const errors = validationResult(req);
-        if(!errors.isEmpty()){
-            console.log(errors);
-            return res.render('pages/index',{"erros": errors, "valores": req.body, "retorno":null});
-        }
 
-        return res.render("pages/relatorios", {"erros":null, "valores":req.body, "retorno":req.body})
-    }
- )
 
 
 
